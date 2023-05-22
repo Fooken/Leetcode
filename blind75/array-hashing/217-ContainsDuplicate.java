fd5c2ebd74f0872075b46c02e636e4a722db7711
Kai Fu <kkaifu@amazon.com>
2023-05-21 17:37:36 -0700

    Add first file in git

---
+++ blind75/array-hashing/217-ContainsDuplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    // DS: HashTable: HashSet T:O(n)
+    // check: set.add();
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
